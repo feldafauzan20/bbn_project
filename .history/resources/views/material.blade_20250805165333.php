339189ff1c31b1b99cdff91673d7ba24597350d6
feldafauzan20 <feldafauzan20@gmail.com>
2025-08-05 20:33:16 +0700

    add: material page

---
+++ .history/resources/views/material.blade_20250805165333.php
@@ -0,0 +1,77 @@
+<x-layouts.main-layout>
+    <x-slot:title>
+        Cara Kami Bekerja | BIMA BAHAMA NUSANTARA
+    </x-slot:title>
+
+    {{-- NAVBAR START --}}
+    <x-navbar-bg-white />
+    {{-- NAVBAR END --}}
+
+    {{-- HERO START --}}
+    <section class="lg:flex lg:gap-x-12" id="hero-section">
+        <div class="mx-5 lg:mx-0 lg:ml-36 2xl:ml-61 pt-40 ">
+            <div>
+                <p class="text-sky-700 text-lg font-semibold tracking-tightest leading-relaxed">Material</p>
+            </div>
+            <div class="w-96 lg:w-160 my-5 md:my-8.5 lg:my-3.5">
+                <h2
+                    class="text-black text-5xl lg:text-7xl leading-[49.92px] lg:leading-18 font-semibold tracking-tightest">
+                    Material Terbaik untuk Hasil Terbaik</h2>
+            </div>
+            <div class="w-96 md:w-160 mb-15 md:mb-20 lg:mb-37">
+                <p class="text-black text-lg font-normal leading-relaxed tracking-tightest">Kami hanya menggunakan
+                    material unggulan yang telah teruji untuk memastikan hasil konstruksi yang kuat, tahan lama, dan
+                    sesuai standar teknis.</p>
+            </div>
+        </div>
+        <div class="lg:bg-blue-400">
+            <img src="{{ asset('images/works/img-building-hero-works.webp') }}" class="object-cover lg:h-full"
+                alt="image building hero">
+        </div>
+    </section>
+    {{-- HERO END --}}
+
+    {{-- MATERIAL START --}}
+    <section>
+        <div class="bg-red-400 mx-5 mt-12 relative">
+            <div class="flex w-96 flex-wrap gap-y-2.5 gap-x-15 bg-blue-600">
+                <a href="" class="tracking-tightest text-lg leading-relaxed text-black">Material Komofloor</a>
+                <a href="" class="tracking-tightest text-lg leading-relaxed text-black">Material Komowrap</a>
+                <a href="" class="tracking-tightest text-lg leading-relaxed text-black">Material Komoproof</a>
+                <a href="" class="tracking-tightest text-lg leading-relaxed text-black">Mesin dan
+                    Perlengkapan</a>
+                <a href="" class="tracking-tightest text-lg leading-relaxed text-black">Perkuatan Struktur</a>
+                <a href="" class="tracking-tightest text-lg leading-relaxed text-black">Mata Gerinda</a>
+            </div>
+
+            {{-- Komofloor --}}
+            <div class="grid grid-cols-1 md:grid-cols-2 relative lg:grid-cols-3">
+                <div class="bg-stone-50 rounded-2xl">
+                    <div class="relative">
+                        <!-- Background image -->
+                        <div class="absolute inset-0 bg-cover bg-center"
+                            style="background-image: url('/images/welcome/bg-big-hero.webp');">
+                        </div>
+                        <!-- Overlay gradient -->
+                        <div class="absolute inset-0 bg-gradient-to-b from-black/0 to-black/50"></div>
+                        <!-- Konten lain di sini jika perlu -->
+                        <p>lorem10</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- MATERIAL END --}}
+
+    {{-- FOOTER COMPONENT START --}}
+    <section class="mx-5 lg:mx-36 2xl:mx-61 mt-20.5 md:mt-20 lg:mt-26">
+        <x-footer-component />
+    </section>
+    {{-- FOOTER COMPONENT END --}}
+
+    {{-- MY JS START --}}
+    @push('scripts')
+        @vite(['resources/js/material.js'])
+    @endpush
+    {{-- MY JS END --}}
+</x-layouts.main-layout>
